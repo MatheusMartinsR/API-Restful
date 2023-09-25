26fa1ef84ced9b43b13d5009dd8ee7657ad5c960
MatheusMartinsR <martinsmatheusrh08@gmail.com>
2023-09-25 20:16:27 -0300

    full project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+app.use(express.json())
+
+const personRoutes = require('./routes/personRoutes')
+app.use('/person', personRoutes)
+
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'ola express!'})
+})
+
+
+mongoose.connect('mongodb+srv://MatheusMartinsR:Qu6GiAJnycWYVQ1z@cluster0.zq8g3mn.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log("Conectado ao banco de dados")
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
+
+
+app.listen(3001)
